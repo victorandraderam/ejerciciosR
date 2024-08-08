20475ef9c71e21ef0f20597edcc5c0edd5b69398
Víctor Andrade <contacto@victorandrade.cl>
2024-08-07 23:52:53 -0400

    full com

--- ejerciciotaller2.R
+++ ejerciciotaller2.R
@@ -4,3 +4,15 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(stringr)
+library(fs)
+library(purrr)
+cargar_archivos_excel <- function(archivoscomb) {
+  archivos_excel <- dir_ls(archivoscomb, recurse = T, glob = "*.xlsx")
+  datos_combinados <- archivos_excel %>%
+    map_dfr(~ read_excel(.x))
+  
+  return(datos_combinados)
+}  
+archivoscomb <- "./T8"
+
+fulldata <- cargar_archivos_excel(archivoscomb)
